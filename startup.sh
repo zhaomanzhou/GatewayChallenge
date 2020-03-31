da78249231ef41f0e04fb5e12b5c538d7106d8f9
zmz <zmzsstreet@163.com>
2020-03-31 10:57:57 +0800

    初赛提交final版本

--- startup.sh
+++ startup.sh
@@ -2,4 +2,4 @@
 # first arg   ThreadNum
 # second arg  ByteBufferPoolSize
 # thrid arg   Whether show log, if it's not none, the log will be printed
-java -jar GatewayChallenge-1.0.jar 10
+java -jar GatewayChallenge-1.0.jar 10 1000
